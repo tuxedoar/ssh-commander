e1878a056a8be769b2d23e4e2bde7482f2170c8a
tuxedoar <tuxedoar@gmail.com>
2020-12-26 22:20:52 -0300

    Modify ssh_commander for using ssh_session module

--- ssh_commander/ssh_commander.py
+++ ssh_commander/ssh_commander.py
@@ -16,20 +16,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from ssh_key_helper import get_ssh_homedir_content
-from ssh_key_helper import check_ssh_keys_exist
-from ssh_key_helper import should_ask_password
+from ssh_session import setup_ssh_session_args
+from ssh_session import start_ssh_session
+from ssh_session import manage_ssh_session
 import argparse
 import sys
 import re
-import getpass
-import itertools
 import logging
 import concurrent.futures
-from socket import error
-from time import sleep
 import coloredlogs
-import paramiko
-from colorama import init, Fore
+from colorama import init
 from _version import __version__
 
 
@@ -68,30 +64,6 @@ def start_multithreaded_sessions(nworkers, target_hosts, ssh_session_args, cmd):
     return None
 
 
-
-
-def setup_ssh_session_args(args):
-    """ Setup needed SSH session arguments """
-    check_home_ssh_keys = check_ssh_keys_exist()
-    ssh_key_file = args.identity_file if args.identity_file else None
-    pw = None
-    ask_for_password = should_ask_password(ssh_key_file)
-    if ask_for_password:
-        logging.info("[+] No SSH key was found nor specified . " \
-                         "Won't try key based authentication!")
-        pw = getpass.getpass('\n Please, enter your password to access hosts: ')
-    else:
-        logging.info("[+] Trying key based authentication!")
-    ssh_session_args = (
-                        args.USER, \
-                        pw, \
-                        args.port, \
-                        ssh_key_file, \
-                        check_home_ssh_keys
-                        )
-    return ssh_session_args
-
-
 def menu_handler():
     """ Setup CLI arguments """
     parser = argparse.ArgumentParser(
@@ -108,79 +80,6 @@ def menu_handler():
                         help='Show current version')
     args = parser.parse_args()
     return args
-
-
-def start_ssh_session(ssh_session, remote_host, session_args):
-    """ Initialize SSH session with remote host """
-    user, pw, port, ssh_key_file, check_home_ssh_keys = \
-                                    session_args[0], \
-                                    session_args[1], \
-                                    session_args[2], \
-                                    session_args[3], \
-                                    session_args[4]
-
-    try:
-        ssh_session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        if ssh_key_file is None and check_home_ssh_keys is not True:
-            # Try password based auth!
-            logging.info("[+] Trying password based auth for host: %s", remote_host)
-            ssh_session.connect(remote_host, username=user, password=pw, port=port, \
-                                look_for_keys=False, allow_agent=False, timeout=10)
-        else:
-            # Try key based auth
-            logging.info("[+] Trying key based auth for host: %s", remote_host)
-            ssh_session.connect(remote_host, username=user, port=port, timeout=10, \
-                                key_filename=ssh_key_file)
-    except (
-            paramiko.ssh_exception.AuthenticationException,
-            paramiko.SSHException,
-            IOError,
-            error
-            ) as e:
-                logging.critical("Failed to connect to host %s: %s",
-                                    remote_host, e)
-                sys.exit(1)
-    return ssh_session
-
-
-def manage_ssh_session(session_args, remote_host, commands):
-    """ Setup the SSH session and exec commands at remote hosts """
-    with paramiko.SSHClient() as my_session:
-        ssh_session = start_ssh_session(my_session, remote_host, session_args)
-        remote_shell = ssh_session.invoke_shell()
-        hosts_output = exec_remote_commands(commands, remote_shell)
-        show_hosts_output(hosts_output, remote_host)
-
-
-def exec_remote_commands(commands, remote_shell):
-    """ Execute provided commands on remote hosts! """
-    hosts_cmds_output = []
-    remote_commands = commands.split(',')
-
-    for command in remote_commands:
-        # Remove double quotes!.
-        command = command.replace('\"', '')
-        remote_shell.send(command+'\n')
-        # Wait 1 second before sending commands provided by user!.
-        sleep(1)
-        # Ouput buffer size in bytes!.
-        output = remote_shell.recv(8000)
-        # Split each line for output.
-        output = output.splitlines()
-        # Store each remote host output!
-        hosts_cmds_output.append(output)
-
-    # Flatten nested lists in "hosts_cmds_output"!
-    flatten_hosts_output = itertools.chain.from_iterable(hosts_cmds_output)
-
-    return flatten_hosts_output
-
-
-def show_hosts_output(hosts_output, target_host):
-    """ Show output results for each target host """
-    logging.info(Fore.CYAN + "[*] Showing output for host %s ...\n", target_host)
-    for output in hosts_output:
-        print(output.decode())
 
 
 def validate_ip_addr(ip_addr):
